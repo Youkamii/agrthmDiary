3ebb7c8936ec143a545eeb3babb1bda5e17f9257
Youkamii <gkfkd747@naver.com>
2023-11-12 22:28:19 +0900

    3진법 뒤집기

--- src/Solutions.java
+++ src/Solutions.java
@@ -819,6 +819,43 @@ public class Solutions {
 		return n;
 	}
 
+	//3진법 뒤집기 ////
+	//1113
+	public static int solution36(int n) {
+		int answer = 0;
+
+		List<Integer> ternary = new ArrayList<>();
+
+		while (n > 0) {
+			ternary.add(n % 3);
+			n /= 3;
+		}
+
+		int[] arrayTernary = ternary.stream().mapToInt(i -> i).toArray();
+
+		int base = 0;
+		for (int i = arrayTernary.length  - 1; i >= 0; i--)
+			answer += arrayTernary[i] * (int)Math.pow(3, base++);
+
+		return answer;
+	}
+
+	public static int solution36_1(int n) {
+		int answer = 0;
+
+		List<Integer> ternary = new ArrayList<>();
+
+		while (n > 0) {
+			ternary.add(n % 3);
+			n /= 3;
+		}
+
+		int base = 0;
+		for (int i = ternary.size() - 1; i >= 0; i--)
+			answer += ternary.get(i) * (int)(Math.pow(3, base++));
+
+		return answer;
+	}
 }
 
 
