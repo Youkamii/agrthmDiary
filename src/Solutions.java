dd0a253ed2310a81ed64315fe5e8882f1fbb716e
Youkamii <gkfkd747@naver.com>
2023-11-12 20:54:51 +0900

    행렬의 덧셈

--- src/Solutions.java
+++ src/Solutions.java
@@ -774,6 +774,20 @@ public class Solutions {
 		return answer;
 	}
 
+	//행렬의 덧셈
+	//1113
+	public int[][] solution34(int[][] arr1, int[][] arr2) {
+		int[][] answer = arr1;
+
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[i].length; j++) {
+				answer[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+
+		return answer;
+	}
+
 }
 
 
