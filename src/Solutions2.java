bfb8a8c53c809eeaa1880f39b591e8203ded2e87
Youkamii <gkfkd747@naver.com>
2023-11-29 22:31:04 +0900

    push from Mac

--- src/Solutions2.java
+++ src/Solutions2.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Solutions2 {
@@ -221,6 +223,120 @@ public class Solutions2 {
 
 		return answer.toString();
 	}
+
+
+	// 바탕화면 정리
+	public int[] solution75(String[] wallpaper) {
+		int min_x = Integer.MAX_VALUE;
+		int min_y = Integer.MAX_VALUE;
+		int max_x = -1;
+		int max_y = -1;
+
+		for (int i = 0; i < wallpaper.length; i++) {
+			for (int j = 0; j < wallpaper[i].length(); j++) {
+				if (wallpaper[i].charAt(j) == '#') {
+					min_x = Math.min(min_x, i);
+					min_y = Math.min(min_y, j);
+					max_x = Math.max(max_x, i);
+					max_y = Math.max(max_y, j);
+				}
+			}
+		}
+
+		max_x += 1;
+		max_y += 1;
+
+		return new int[]{min_x, min_y, max_x, max_y};
+	}
+
+	//개인정보 수집 유효기간
+	public int[] solution76(String today, String[] terms, String[] privacies) {
+		List<Integer> answer = new ArrayList<>();
+
+
+		// today	terms	privacies	result
+		//"2022.05.19"	["A 6", "B 12", "C 3"]	["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]	[1, 3]
+		//"2020.01.01"	["Z 3", "D 5"]	["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]	[1, 4, 5]
+
+		String[][] termsArray = new String[terms.length][2];
+		String[][] privaciesArray = new String[privacies.length][4];
+
+		for (int i = 0; i < terms.length; i++) {
+			termsArray[i] = terms[i].split(" ");
+		}
+
+		for (int i = 0; i < privacies.length; i++) {
+			privaciesArray[i][0] = String.valueOf(privacies[i].charAt(11));
+			privaciesArray[i][1] = privacies[i].substring(0, 10); // 날짜 문자열 직접 저장
+		}
+
+		for (int i = 0; i < privaciesArray.length; i++) {
+			for (int j = 0; j < termsArray.length; j++) {
+				if (privaciesArray[i][0].equals(termsArray[j][0])) { // 문자열 비교는 equals 사용
+					if (checkDDay(today, termsArray[j], privaciesArray[i][1]))
+						answer.add(i + 1);
+				}
+			}
+		}
+
+		int[] answerArray = new int[answer.size()];
+		for (int i = 0; i < answer.size(); i++)
+			answerArray[i] = answer.get(i);
+
+		return answerArray;
+	}
+
+	public boolean checkDDay(String today, String[] termsArray, String privaciesDateStr) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+		LocalDate todayDate = LocalDate.parse(today, formatter);
+		LocalDate privacyDate = LocalDate.parse(privaciesDateStr, formatter);
+
+		int monthsToAdd = Integer.parseInt(termsArray[1]);
+		LocalDate expiryDate = privacyDate.plusMonths(monthsToAdd);
+
+		return expiryDate.isBefore(todayDate) || expiryDate.isEqual(todayDate);
+	}
+
+
+	// 달리기 경주
+	public String[] solution77(String[] players, String[] callings) {
+		List<String> answer = new ArrayList<>();
+		Map<Integer, String> rankPlayers = new HashMap<>();
+		int tmpRank;
+		String tmpName;
+
+		for (int i = 0; i < players.length; i++)
+			rankPlayers.put(i + 1, players[i]);
+
+		for (String name : callings) {
+			tmpName = f
+		}
+
+		for (int i = 0; i < rankPlayers.size(); i++)
+			answer.add(rankPlayers.get);
+
+		String[] arrayAnswer = new String[answer.size()];
+		for (int i = 0; i < arrayAnswer.length; i++)
+			arrayAnswer[i] = answer.get(i);
+
+		return arrayAnswer;
+	}
+
+	public String fullName (String[] players, String initialName) {
+
+		String fullName = "";
+
+		for (String name : players) {
+			if (name.startsWith(initialName))
+				fullName = name;
+		}
+		return fullName;
+
+	}
+
+
+
+
 
 }
 
