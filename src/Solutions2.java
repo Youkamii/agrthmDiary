d2f1ca6312b9ae39adda0ab0d149de9af05c4bc9
Youkamii <gkfkd747@naver.com>
2023-11-30 12:57:51 +0900

    개인정보 수집 유효기간

--- src/Solutions2.java
+++ src/Solutions2.java
@@ -253,7 +253,6 @@ public class Solutions2 {
 	public int[] solution76(String today, String[] terms, String[] privacies) {
 		List<Integer> answer = new ArrayList<>();
 
-
 		// today	terms	privacies	result
 		//"2022.05.19"	["A 6", "B 12", "C 3"]	["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]	[1, 3]
 		//"2020.01.01"	["Z 3", "D 5"]	["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]	[1, 4, 5]
@@ -267,14 +266,14 @@ public class Solutions2 {
 
 		for (int i = 0; i < privacies.length; i++) {
 			privaciesArray[i][0] = String.valueOf(privacies[i].charAt(11));
-			privaciesArray[i][1] = privacies[i].substring(0, 10); // 날짜 문자열 직접 저장
+			privaciesArray[i][1] = privacies[i].substring(0, 10);
 		}
 
 		for (int i = 0; i < privaciesArray.length; i++) {
 			for (int j = 0; j < termsArray.length; j++) {
-				if (privaciesArray[i][0].equals(termsArray[j][0])) { // 문자열 비교는 equals 사용
-					if (checkDDay(today, termsArray[j], privaciesArray[i][1]))
-						answer.add(i + 1);
+				if (privaciesArray[i][0].equals(termsArray[j][0])) {
+					if (checkDDay(today, termsArray[j][1], privaciesArray[i][1]))
+						answer.add(i + 1); // 1부터 시작
 				}
 			}
 		}
@@ -286,13 +285,12 @@ public class Solutions2 {
 		return answerArray;
 	}
 
-	public boolean checkDDay(String today, String[] termsArray, String privaciesDateStr) {
+	public boolean checkDDay(String today, String termsArray, String privaciesDateStr) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
 		LocalDate todayDate = LocalDate.parse(today, formatter);
 		LocalDate privacyDate = LocalDate.parse(privaciesDateStr, formatter);
 
-		int monthsToAdd = Integer.parseInt(termsArray[1]);
-		LocalDate expiryDate = privacyDate.plusMonths(monthsToAdd);
+		LocalDate expiryDate = privacyDate.plusMonths(Integer.parseInt(termsArray));
 
 		return expiryDate.isBefore(todayDate) || expiryDate.isEqual(todayDate);
 	}
